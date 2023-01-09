74652d9a3ee53ab2fb22466fb69cd797404e4225
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-09 18:58:00 +0500

    file

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  resources :xurs
+  resources :tds
+
+  root "projects#index"
+
+  devise_for :users
+  
+  resources :mboards
+  
+  resources :mbs
+  
+  resources :projects do
+    
+    resources :mboards
+
+    resources :tds
+    
+    resources :xurs
+    
+  end
+  
+end
