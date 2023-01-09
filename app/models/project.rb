6933dd0738d7d14c9e745f2cb1822776bd6c0596
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-09 18:13:05 +0500

    file

---
+++ app/models/project.rb
@@ -0,0 +1,10 @@
+class Project < ApplicationRecord
+  belongs_to :user
+  has_many :mboards, dependent: :destroy
+  has_many :tds, dependent: :destroy
+  has_many :xurs, dependent: :destroy
+
+
+  validates :title, presence: true
+  validates :body, presence: true
+end
